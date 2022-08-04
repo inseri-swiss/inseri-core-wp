6b059f7b7505cbab4542b111e0953f8f056c6075
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2022-08-04 17:38:35 +0200

    build: add new blocks automatically to build / register cycle

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
 const defaultConfig = require('@wordpress/scripts/config/webpack.config')
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const fs = require('fs')
 
 const tsChecker = new ForkTsCheckerWebpackPlugin({
 	typescript: { diagnosticOptions: { semantic: true, syntactic: true } },
 })
 
+const blockEntrypoints = fs.readdirSync('./src/blocks').reduce(
+	(accumulator, item) => ({
+		...accumulator,
+		[`blocks/${item}/index`]: `./src/blocks/${item}`,
+	}),
+	{}
+)
+
 module.exports = {
 	...defaultConfig,
-	entry: {
-		'blocks/core/index': './src/blocks/core',
-	},
+	entry: { ...blockEntrypoints },
 	plugins: [...defaultConfig.plugins, tsChecker],
 }
