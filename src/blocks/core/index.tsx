7c50f9b99a37ecdd5643017872c4d29ba4d56702
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2022-08-04 19:03:42 +0200

    style: fix linting issue

--- src/blocks/core/index.tsx
+++ src/blocks/core/index.tsx
@@ -3,12 +3,13 @@ import './style.scss'
 import json from './block.json'
 import edit from './edit'
 import save from './save'
+import { BlockIcon } from 'wordpress__blocks'
 
 const { name, icon, ...settings } = json
 
 registerBlockType<any>(name, {
 	...settings,
-	icon: icon as any,
+	icon: icon as BlockIcon,
 	edit,
 	save,
 })
