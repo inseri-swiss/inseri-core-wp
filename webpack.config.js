ec6d1809de104b05bd2b0d97d241adab727c20ea
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2022-08-10 17:52:18 +0200

    build: extract global styling into single file and preserve wp-generated classes

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,28 @@
 const defaultConfig = require('@wordpress/scripts/config/webpack.config')
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts')
 const fs = require('fs')
 
 const tsChecker = new ForkTsCheckerWebpackPlugin({
 	typescript: { diagnosticOptions: { semantic: true, syntactic: true } },
 })
 
+const initialEntrypoints = { global: './src/global.scss' }
+
 const blockEntrypoints = fs.readdirSync('./src/blocks').reduce(
 	(accumulator, item) => ({
 		...accumulator,
 		[`blocks/${item}/index`]: `./src/blocks/${item}`,
 	}),
-	{}
+	initialEntrypoints
 )
 
 module.exports = {
 	...defaultConfig,
 	entry: { ...blockEntrypoints },
-	plugins: [...defaultConfig.plugins, tsChecker],
+	plugins: [
+		...defaultConfig.plugins,
+		tsChecker,
+		new RemoveEmptyScriptsPlugin(),
+	],
 }
