25fabc4de6b4b1f09b237dfcc3582c7319510ce5
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2022-08-17 11:48:03 +0200

    build: setup react-aria

--- src/blocks/core/edit.tsx
+++ src/blocks/core/edit.tsx
@@ -1,10 +1,19 @@
 import { __ } from '@wordpress/i18n'
 import { useBlockProps } from '@wordpress/block-editor'
 import './editor.scss'
+import { Button } from '../../component-library/button'
 
 export default function Edit() {
 	return (
 		<p {...useBlockProps()}>
+			<Button
+				onPress={() => {
+					console.log('Button pressed') // eslint-disable-line no-console
+				}}
+				className={'inseri-is-danger'}
+			>
+				A11y click
+			</Button>
 			{__('Inseri Core – hello from the editor!', 'core')}
 		</p>
 	)
