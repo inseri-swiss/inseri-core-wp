eb851c72ffa392dacb5606797030d45f373fe52a
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2022-09-08 14:38:58 +0200

    refactor: rewrite rest api functions using Either

--- includes/rest_api.php
+++ includes/rest_api.php
@@ -2,9 +2,11 @@
 namespace inseri_core\rest;
 
 use inseri_core\db as db;
+use inseri_core\Either;
+use \WP_REST_Response;
 
 define('NS', 'inseri/v1');
-define('ROUTE_DATASOURCES', '/datasources/');
+define('BASE_ROUTE', '/datasources/');
 
 function register_api_routes() {
 	$routes = [
@@ -17,7 +19,7 @@ function register_api_routes() {
 	];
 
 	foreach ($routes as $r) {
-		register_rest_route(NS, ROUTE_DATASOURCES . $r[1], [
+		register_rest_route(NS, BASE_ROUTE . $r[1], [
 			'methods' => $r[0],
 			'callback' => 'inseri_core\rest\\' . $r[2],
 			'permission_callback' => '__return_true',
@@ -25,96 +27,90 @@ function register_api_routes() {
 	}
 }
 
-function validate_datasource($body, $check_id = false) {
+function validate_datasource($body, $check_id = false): Either {
 	$non_empty_fields = ['description', 'method', 'url'];
 
 	if ($check_id) {
 		array_push($non_empty_fields, 'id');
 	}
 
-	foreach ($non_empty_fields as $field) {
-		if (empty($body[$field])) {
-			return new \WP_Error(
-				'missing_required_field',
-				"missing required field: '$field'",
-				[
-					'status' => 400,
-				]
-			);
-		}
+	$missing_fields = array_filter(
+		$non_empty_fields,
+		fn($field) => empty($body[$field])
+	);
+
+	if (empty($missing_fields)) {
+		return Either::Right($body);
 	}
 
-	return null;
+	$error_msg = 'missing required field: ' . implode(', ', $missing_fields);
+	return Either::Left($error_msg);
 }
 
-/*
- * $either is expected to be 'Array(null, result)' or 'Array(error_msg, null)'
- */
-function unpack_db_result($either, $successful_code = 200, $fail_code = 400) {
-	if (!empty($either[0])) {
-		return new \WP_REST_Response($either[0], $fail_code);
+function handle_left($value, $custom_codes = []) {
+	$default_codes = [
+		'execution-failed' => ['internal server error', 500],
+		'not-modified' => ['bad request', 400],
+		'not-found' => ['not found', 404],
+	];
+
+	if (array_key_exists($value, $default_codes)) {
+		$code = $custom_codes[$value] ?? $default_codes[$value];
+		return new WP_REST_Response($code[0], $code[1]);
 	}
 
-	return new \WP_REST_Response($either[1], $successful_code);
+	return new WP_REST_Response($value, 400);
 }
 
 function get_all($request) {
-	$result = db\get_all();
-	return unpack_db_result($result, 200, 500);
+	return db\get_all()->fold(
+		fn($error) => handle_left($error),
+		fn($item) => new WP_REST_Response($item, 200)
+	);
 }
 
 function get_one($request) {
 	$id = $request['id'];
-	$result = db\get_one($id);
-	return unpack_db_result($result, 200, 404);
+
+	return db\get_one($id)->fold(
+		fn($error) => handle_left($error),
+		fn($item) => new WP_REST_Response($item, 200)
+	);
 }
 
 function post($request) {
 	$body = $request->get_json_params();
 
-	$validation_result = validate_datasource($body);
-	if (is_wp_error($validation_result)) {
-		return $validation_result;
-	}
-
-	$result = db\insert_one($body);
-	if (!empty($result[0])) {
-		return new \WP_REST_Response($result[0], 400);
-	}
-
-	$result = db\get_one($result[1]);
-	return unpack_db_result($result, 201, 400);
+	return validate_datasource($body)
+		->flatMap(fn($item) => db\insert_one($item))
+		->flatMap(fn($new_id) => db\get_one($new_id))
+		->fold(
+			fn($error) => handle_left($error),
+			fn($item) => new WP_REST_Response($item, 201)
+		);
 }
 
 function put($request) {
 	$body = $request->get_json_params();
+	$id = $body['id'];
+	$custom_codes = ['not-modified' => ['successfully updated', 200]];
 
-	$validation_result = validate_datasource($body, true);
-	if (is_wp_error($validation_result)) {
-		return $validation_result;
-	}
-
-	$result = db\update_one($body);
-	if (!empty($result[0])) {
-		return new \WP_REST_Response($result[0], 400);
-	}
-
-	$result = db\get_one($body['id']);
-	return unpack_db_result($result, 200, 400);
+	return validate_datasource($body, true)
+		->flatMap(fn($incoming) => db\get_one($id)->map(fn($_) => $incoming))
+		->flatMap(fn($item) => db\update_one($item))
+		->fold(
+			fn($error) => handle_left($error, $custom_codes),
+			fn($_) => new WP_REST_Response('successfully updated', 200)
+		);
 }
 
 function delete($request) {
 	$id = $request['id'];
-	$get_result = db\get_one($id);
 
-	if (!empty($get_result[0])) {
-		return new \WP_REST_Response($get_result[0], 404);
-	}
-
-	$result = db\delete_one($id);
-	if (!empty($result[0])) {
-		return new \WP_REST_Response($result[0], 500);
-	}
-
-	return new \WP_REST_Response($get_result[1], 200);
+	return db\get_one($id)
+		->flatMap(fn($item) => db\delete_one($id)->map(fn($_) => $item))
+		->fold(
+			fn($error) => handle_left($error),
+			fn($item) => new WP_REST_Response($item, 200)
+		);
 }
