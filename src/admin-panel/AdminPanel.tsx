4d1a38892bfbfd41a31ed56ac7bb4f1fded41e58
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2022-09-20 10:36:41 +0200

    style: increase printWidth

--- src/admin-panel/AdminPanel.tsx
+++ src/admin-panel/AdminPanel.tsx
@@ -6,13 +6,9 @@ export default () => {
 			<div className={bulma`container is-fluid`}>
 				<div className={bulma`my-4 is-flex is-align-items-center`}>
 					<h1 className={bulma`is-size-4`}>{i18n`Datasources - Inseri`}</h1>
-					<button className={bulma`admin-button has-text-weight-bold ml-3`}>
-						{i18n`Add New`}
-					</button>
+					<button className={bulma`admin-button has-text-weight-bold ml-3`}>{i18n`Add New`}</button>
 				</div>
-				<div
-					className={bulma`pt-3 my-3 is-flex is-justify-content-space-between`}
-				>
+				<div className={bulma`pt-3 my-3 is-flex is-justify-content-space-between`}>
 					<div>
 						<div className={bulma`admin-select`}>
 							<select>
@@ -26,20 +22,14 @@ export default () => {
 								<option>With options</option>
 							</select>
 						</div>
-						<button className={bulma`admin-button has-text-weight-bold`}>
-							{i18n`Filter`}
-						</button>
+						<button className={bulma`admin-button has-text-weight-bold`}>{i18n`Filter`}</button>
 					</div>
 					<div className={bulma`is-flex`}>
 						<input className={bulma`admin-input mx-2`} type="text" />
-						<button className={bulma`admin-button has-text-weight-bold`}>
-							{i18n`Search`}
-						</button>
+						<button className={bulma`admin-button has-text-weight-bold`}>{i18n`Search`}</button>
 					</div>
 				</div>
-				<table
-					className={bulma`table is-fullwidth is-striped is-bordered is-size-6`}
-				>
+				<table className={bulma`table is-fullwidth is-striped is-bordered is-size-6`}>
 					<thead>
 						<tr>
 							<th>{i18n`Name`}</th>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,7 @@
 import { __ } from '@wordpress/i18n'
 
 export function i18n(parts: TemplateStringsArray, ..._keys: any[]) {
-	return __(parts.join(''), 'inseri-core')
+	return __(parts.join(''), 'inseri-core') // eslint-disable-line
 }
 
 export function bulma(parts: TemplateStringsArray, ..._keys: any[]) {
