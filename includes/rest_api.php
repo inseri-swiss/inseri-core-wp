4bca7b28e636cf0f17635feb2a65b90d0c2b12c5
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2022-10-22 18:23:45 +0200

    feat: return author_name on get request

--- includes/rest_api.php
+++ includes/rest_api.php
@@ -1,145 +1,149 @@
 <?php
-namespace inseri_core\rest;
-
-use inseri_core\db as db;
 use inseri_core\Either;
-use \WP_REST_Response;
 
-function register_api_routes() {
-	$namespace = 'inseri/v1';
-	$base_route = '/datasources/';
+class Inseri_Core_Rest {
+	private $inseri_db;
+	private $namespace = 'inseri/v1';
+	private $base_route = '/datasources/';
 
-	$routes = [
-		// [method, endpoint_suffix, callback, permission capability]
-		['GET', '', 'get_all', ''],
-		['GET', '(?P<item_id>[\d]+)', 'get_one', ''],
-		['POST', '', 'post', 'publish_posts'],
-		['PUT', '(?P<item_id>[\d]+)', 'put', 'publish_posts'],
-		['DELETE', '(?P<item_id>[\d]+)', 'delete', 'delete_others_posts'],
-	];
-
-	foreach ($routes as $r) {
-		register_rest_route($namespace, $base_route . $r[1], [
-			'methods' => $r[0],
-			'callback' => 'inseri_core\rest\\' . $r[2],
-			'permission_callback' => fn($request) => validate_permission($request, $r[0]),
-		]);
+	function __construct($inseri_db) {
+		$this->inseri_db = $inseri_db;
 	}
-}
 
-function validate_permission($request, $method): bool {
-	switch (strtoupper($method)) {
-		case 'GET':
-			return true;
-		case 'POST':
-			return current_user_can('publish_posts');
-		case 'PUT':
-			$item_id = $request['item_id'];
+	function register_api_routes() {
+		$routes = [
+			// [method, endpoint_suffix, callback, permission capability]
+			['GET', '', 'get_all', ''],
+			['GET', '(?P<item_id>[\d]+)', 'get_one', ''],
+			['POST', '', 'post', 'publish_posts'],
+			['PUT', '(?P<item_id>[\d]+)', 'put', 'publish_posts'],
+			['DELETE', '(?P<item_id>[\d]+)', 'delete', 'delete_others_posts'],
+		];
 
-			$handle_right = function ($item) {
-				if ($item->author == get_current_user_id()) {
-					return current_user_can('publish_posts');
-				}
-
-				return current_user_can('edit_others_posts');
-			};
-
-			return db\get_one($item_id)->fold(fn($not_found) => true, $handle_right);
-
-		case 'DELETE':
-			$item_id = $request['item_id'];
-
-			$handle_right = function ($item) {
-				if ($item->author == get_current_user_id()) {
-					return current_user_can('delete_posts');
-				}
-
-				return current_user_can('delete_others_posts');
-			};
-
-			return db\get_one($item_id)->fold(fn($not_found) => true, $handle_right);
-
-		default:
-			return true;
-	}
-}
-
-function validate_datasource($request, $check_id = false): Either {
-	$body = $request->get_json_params();
-	$non_empty_fields = ['description', 'type', 'method', 'url'];
-	$json_fields = ['headers', 'query_params'];
-
-	foreach ($json_fields as $field) {
-		if (isset($body[$field])) {
-			$body[$field] = json_encode($body[$field]);
+		foreach ($routes as $r) {
+			register_rest_route($this->namespace, $this->base_route . $r[1], [
+				'methods' => $r[0],
+				'callback' => [$this, $r[2]],
+				'permission_callback' => fn($request) => $this->validate_permission($request, $r[0]),
+			]);
 		}
 	}
 
-	if ($check_id) {
-		if ($request['item_id'] != $request->get_json_params()['id']) {
-			return Either::Left('The id from URL does not match with id in body');
+	function validate_permission($request, $method): bool {
+		switch (strtoupper($method)) {
+			case 'GET':
+				return true;
+			case 'POST':
+				return current_user_can('publish_posts');
+			case 'PUT':
+				$item_id = $request['item_id'];
+
+				$handle_right = function ($item) {
+					if ($item->author == get_current_user_id()) {
+						return current_user_can('publish_posts');
+					}
+
+					return current_user_can('edit_others_posts');
+				};
+
+				return $this->inseri_db->get_one($item_id)->fold(fn($not_found) => true, $handle_right);
+
+			case 'DELETE':
+				$item_id = $request['item_id'];
+
+				$handle_right = function ($item) {
+					if ($item->author == get_current_user_id()) {
+						return current_user_can('delete_posts');
+					}
+
+					return current_user_can('delete_others_posts');
+				};
+
+				return $this->inseri_db->get_one($item_id)->fold(fn($not_found) => true, $handle_right);
+
+			default:
+				return true;
 		}
 	}
 
-	$missing_fields = array_filter($non_empty_fields, fn($field) => empty($body[$field]));
+	function validate_datasource($request, $check_id = false): Either {
+		$body = $request->get_json_params();
+		$non_empty_fields = ['description', 'type', 'method', 'url'];
+		$json_fields = ['headers', 'query_params'];
 
-	if (empty($missing_fields)) {
-		return Either::Right($body);
+		foreach ($json_fields as $field) {
+			if (isset($body[$field])) {
+				$body[$field] = json_encode($body[$field]);
+			}
+		}
+
+		if ($check_id) {
+			if ($request['item_id'] != $request->get_json_params()['id']) {
+				return Either::Left('The id from URL does not match with id in body');
+			}
+		}
+
+		$missing_fields = array_filter($non_empty_fields, fn($field) => empty($body[$field]));
+
+		if (empty($missing_fields)) {
+			return Either::Right($body);
+		}
+
+		$error_msg = 'missing required field: ' . implode(', ', $missing_fields);
+		return Either::Left($error_msg);
 	}
 
-	$error_msg = 'missing required field: ' . implode(', ', $missing_fields);
-	return Either::Left($error_msg);
-}
+	private function handle_left($value, $custom_codes = []) {
+		$default_codes = [
+			'execution-failed' => ['internal server error', 500],
+			'not-modified' => ['bad request', 400],
+			'not-found' => ['not found', 404],
+		];
 
-function handle_left($value, $custom_codes = []) {
-	$default_codes = [
-		'execution-failed' => ['internal server error', 500],
-		'not-modified' => ['bad request', 400],
-		'not-found' => ['not found', 404],
-	];
+		if (array_key_exists($value, $default_codes)) {
+			$code = $custom_codes[$value] ?? $default_codes[$value];
+			return new WP_REST_Response($code[0], $code[1]);
+		}
 
-	if (array_key_exists($value, $default_codes)) {
-		$code = $custom_codes[$value] ?? $default_codes[$value];
-		return new WP_REST_Response($code[0], $code[1]);
+		return new WP_REST_Response($value, 400);
 	}
 
-	return new WP_REST_Response($value, 400);
-}
+	function get_all($request) {
+		return $this->inseri_db->get_all()->fold(fn($error) => $this->handle_left($error), fn($item) => new WP_REST_Response($item, 200));
+	}
 
-function get_all($request) {
-	return db\get_all()->fold(fn($error) => handle_left($error), fn($item) => new WP_REST_Response($item, 200));
-}
+	function get_one($request) {
+		$id = $request['item_id'];
 
-function get_one($request) {
-	$id = $request['item_id'];
+		return $this->inseri_db->get_one($id)->fold(fn($error) => $this->handle_left($error), fn($item) => new WP_REST_Response($item, 200));
+	}
 
-	return db\get_one($id)->fold(fn($error) => handle_left($error), fn($item) => new WP_REST_Response($item, 200));
-}
+	function post($request) {
+		return $this->validate_datasource($request)
+			->flatMap(function ($item) {
+				$item['author'] = get_current_user_id();
+				return $this->inseri_db->insert_one($item);
+			})
+			->flatMap(fn($new_id) => $this->inseri_db->get_one($new_id))
+			->fold(fn($error) => $this->handle_left($error), fn($item) => new WP_REST_Response($item, 201));
+	}
 
-function post($request) {
-	return validate_datasource($request)
-		->flatMap(function ($item) {
-			$item['author'] = get_current_user_id();
-			return db\insert_one($item);
-		})
-		->flatMap(fn($new_id) => db\get_one($new_id))
-		->fold(fn($error) => handle_left($error), fn($item) => new WP_REST_Response($item, 201));
-}
+	function put($request) {
+		$id = $request['item_id'];
+		$custom_codes = ['not-modified' => ['successfully updated', 200]];
 
-function put($request) {
-	$id = $request['item_id'];
-	$custom_codes = ['not-modified' => ['successfully updated', 200]];
+		return $this->validate_datasource($request, true)
+			->flatMap(fn($incoming) => $this->inseri_db->get_one($id)->map(fn($_) => $incoming))
+			->flatMap(fn($item) => $this->inseri_db->update_one($item))
+			->fold(fn($error) => $this->handle_left($error, $custom_codes), fn($_) => new WP_REST_Response('successfully updated', 200));
+	}
 
-	return validate_datasource($request, true)
-		->flatMap(fn($incoming) => db\get_one($id)->map(fn($_) => $incoming))
-		->flatMap(fn($item) => db\update_one($item))
-		->fold(fn($error) => handle_left($error, $custom_codes), fn($_) => new WP_REST_Response('successfully updated', 200));
-}
+	function delete($request) {
+		$id = $request['item_id'];
 
-function delete($request) {
-	$id = $request['item_id'];
-
-	return db\get_one($id)
-		->flatMap(fn($item) => db\delete_one($id)->map(fn($_) => $item))
-		->fold(fn($error) => handle_left($error), fn($item) => new WP_REST_Response($item, 200));
+		return $this->inseri_db
+			->get_one($id)
+			->flatMap(fn($item) => $this->inseri_db->delete_one($id)->map(fn($_) => $item))
+			->fold(fn($error) => $this->handle_left($error), fn($item) => new WP_REST_Response($item, 200));
+	}
 }
--- inseri-core.php
+++ inseri-core.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Plugin Name:       Inseri Core
+ * Plugin Name:       inseri core
  * Description:       inseri rocks :)
  * Requires at least: 5.8
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            Inseri Swiss
+ * Author:            inseri swiss
  * Author URI:        https://inseri.swiss
  * License:           GPL-3.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
@@ -13,18 +13,18 @@
  *
  * @package           inseri
  *
- * Inseri Core is free software: you can redistribute it and/or modify
+ * inseri core is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Inseri Core is distributed in the hope that it will be useful,
+ * inseri core is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Inseri Core.  If not, see <http://www.gnu.org/licenses/>.
+ * along with inseri core.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 require_once plugin_dir_path(__FILE__) . 'includes/utils.php';
@@ -32,12 +32,16 @@ require_once plugin_dir_path(__FILE__) . 'includes/db.php';
 require_once plugin_dir_path(__FILE__) . 'includes/rest_api.php';
 require_once plugin_dir_path(__FILE__) . 'includes/admin_panel.php';
 
-register_activation_hook(__FILE__, 'inseri_core\db\setup_table');
+global $wpdb;
+$inseri_core_db = new Inseri_Core_DB($wpdb);
+$inseri_core_rest = new Inseri_Core_Rest($inseri_core_db);
+
+register_activation_hook(__FILE__, [$inseri_core_db, 'setup_table']);
 
 /**
  * REST API
  */
-add_action('rest_api_init', 'inseri_core\rest\register_api_routes');
+add_action('rest_api_init', [$inseri_core_rest, 'register_api_routes']);
 
 /**
  * Blocks
