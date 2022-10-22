3e9b4fcda7a6ded0c98d405f763fe0451b5f9df7
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2022-10-22 16:34:14 +0200

    feat: outline edit datasource layout

--- src/admin-panel/AdminPanel.tsx
+++ src/admin-panel/AdminPanel.tsx
@@ -1,3 +1,4 @@
+import { useState } from '@wordpress/element'
 import { PAGES } from './config'
 import { DetailView } from './DetailView'
 import { ListView } from './ListView'
@@ -6,5 +7,15 @@ export function AdminPanel() {
 	const queryParams = new URLSearchParams(document.location.search)
 	const showAddNew = queryParams.get('page') === PAGES['add-new']
 
-	return showAddNew ? <DetailView mode="create" /> : <ListView />
+	const [itemId, setItemId] = useState<number | null>(null)
+
+	if (showAddNew) {
+		return <DetailView mode="create" />
+	}
+
+	if (itemId) {
+		return <DetailView mode="edit" itemId={itemId} />
+	}
+
+	return <ListView onItemClick={setItemId} />
 }
