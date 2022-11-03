7aba1dddea8cce59435a7ff5fa298ee10d9e5b15
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2022-11-03 12:53:57 +0100

    feat: cache availableSource computations

--- src/mainScript.ts
+++ src/mainScript.ts
@@ -1,4 +1,5 @@
 import domReady from '@wordpress/dom-ready'
+import { useMemo } from '@wordpress/element'
 import { nanoid } from 'nanoid/non-secure'
 import create from 'zustand'
 import { devtools } from 'zustand/middleware'
@@ -28,13 +29,14 @@ interface StoreWrapper {
 	blockTypeByHandle: Record<string, string>
 	//             slice          key
 	mainStore: Record<string, Record<string, Field>>
+	totalFields: number
 }
 
 class InseriCore {
 	#useInternalStore
 
 	constructor() {
-		let store: any = (_set: any) => ({ blockTypeByHandle: {}, mainStore: {} })
+		let store: any = (_set: any) => ({ blockTypeByHandle: {}, mainStore: {}, totalFields: 0 })
 
 		if (process.env.NODE_ENV !== 'production') {
 			store = devtools(store, { name: 'inseri-store' })
@@ -55,19 +57,21 @@ class InseriCore {
 	}
 
 	useAvailableSources(contentTypeFilter?: string | ((contentType: string) => boolean)) {
-		const mainStore = this.#useInternalStore((state) => state.mainStore)
+		const totalFields = this.#useInternalStore((state) => state.totalFields)
+		return useMemo(() => {
+			const mainStore = this.#useInternalStore.getState().mainStore
+			let sources: SourceDTO[] = Object.entries(mainStore).flatMap(([handle, slice]) => {
+				let sourcesOfSlice = Object.entries(slice).filter(([_, field]) => field.status !== 'unavailable')
+				if (contentTypeFilter) {
+					const filterByContentType = typeof contentTypeFilter === 'string' ? (ct: string) => ct.includes(contentTypeFilter) : contentTypeFilter
+					sourcesOfSlice = sourcesOfSlice.filter(([_, field]) => filterByContentType(field.contentType))
+				}
 
-		let sources: SourceDTO[] = Object.entries(mainStore).flatMap(([handle, slice]) => {
-			let sourcesOfSlice = Object.entries(slice).filter(([_, field]) => field.status !== 'unavailable')
-			if (contentTypeFilter) {
-				const filterByContentType = typeof contentTypeFilter === 'string' ? (ct: string) => ct.includes(contentTypeFilter) : contentTypeFilter
-				sourcesOfSlice = sourcesOfSlice.filter(([_, field]) => filterByContentType(field.contentType))
-			}
+				return sourcesOfSlice.map(([key, { status, value, ...rest }]) => ({ ...rest, key, slice: handle }))
+			})
 
-			return sourcesOfSlice.map(([key, { status, value, ...rest }]) => ({ ...rest, key, slice: handle }))
-		})
-
-		return sources
+			return sources
+		}, [totalFields, contentTypeFilter])
 	}
 
 	createDispatch = (blockHandle: string, fieldKey: string) => (updateField: Partial<Omit<Field, 'isContentTypeDynamic'>>) => {
@@ -92,6 +96,7 @@ class InseriCore {
 
 			state.blockTypeByHandle[blockHandle] = blockName
 			state.mainStore[blockHandle] = slice
+			state.totalFields += fields.length
 		})
 
 		return blockHandle
@@ -100,9 +105,13 @@ class InseriCore {
 	removeBlock(blockHandle: string) {
 		this.#useInternalStore.setState((state) => {
 			const slice = state.mainStore[blockHandle]
-			Object.keys(slice).forEach((k) => {
+			const fields = Object.keys(slice)
+
+			fields.forEach((k) => {
 				slice[k].status = 'unavailable'
 			})
+
+			state.totalFields -= fields.length
 		})
 	}
 
@@ -110,12 +119,14 @@ class InseriCore {
 		this.#useInternalStore.setState((state) => {
 			const { key, ...rest } = field
 			state.mainStore[blockHandle][key] = { ...rest, status: 'initial' }
+			state.totalFields++
 		})
 	}
 
 	removeField(blockHandle: string, key: string) {
 		this.#useInternalStore.setState((state) => {
 			state.mainStore[blockHandle][key].status = 'unavailable'
+			state.totalFields--
 		})
 	}
 }
