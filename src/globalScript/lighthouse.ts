bec93ba32664c9aa33f5bc4d40993ca3469cdb7a
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2022-11-24 16:09:32 +0100

    feat: redesign lighthouse

--- src/globalScript/lighthouse.ts
+++ src/globalScript/lighthouse.ts
@@ -1,46 +1,55 @@
 import { useEffect, useMemo } from '@wordpress/element'
+import produce, { setAutoFreeze } from 'immer'
 import type { Draft } from 'immer'
-import { setAutoFreeze } from 'immer'
-import { nanoid } from 'nanoid/non-secure'
 import create from 'zustand'
 import { devtools } from 'zustand/middleware'
 import { immer } from 'zustand/middleware/immer'
 
-interface Field {
+interface BeaconState extends BaseBeaconState {
+	description?: string
+	default?: string
+}
+
+interface BaseBeaconState {
 	contentType: string
-	isContentTypeDynamic?: boolean
-	description: string
 	status: 'initial' | 'loading' | 'ready' | 'error' | 'unavailable'
 	error?: string
 	value?: any
 }
-type FieldWithKey = Omit<Field, 'status' | 'value' | 'error'> & {
+
+interface InitBeaconConfig {
 	key: string
+	default?: any
+	contentType: string
+	description: string
 }
 
-interface SourceDTO extends FieldWithKey {
-	slice: string
-	instanceName: string
+interface ProducerBeacon {
+	key: string
+	contentType: string
+	default?: any
+}
+interface ConsumerBeacon extends ProducerBeacon {
+	description: string
 }
 
-interface Meta {
+interface BlockConfig {
 	blockType: string
 	instanceName: string
 }
 
-interface StoreWrapper {
-	//             slice          key
-	mainStore: Record<string, Record<string, Field>>
-	metaByHandle: Record<string, Meta>
-	totalFields: number
+interface InitBlockConfig extends BlockConfig {
+	blockId: string
 }
 
-const generateToken = () => nanoid()
+interface StoreWrapper {
+	blocks: Record<string, BlockConfig>
+	beacons: Record<string, BeaconState>
+}
 
 let store: any = (_set: any) => ({
-	mainStore: {},
-	metaByHandle: {},
-	totalFields: 0,
+	blocks: {},
+	beacons: {},
 })
 
 if (process.env.NODE_ENV !== 'production') {
@@ -52,117 +61,121 @@ if (process.env.NODE_ENV !== 'production') {
 
 const useInternalStore = create(immer<StoreWrapper>(store))
 
-function useInseriStore(source: SourceDTO): Field {
-	const { slice, key, contentType, description } = source
-	return useInternalStore((state) => state.mainStore[slice]?.[key] ?? { contentType, status: 'initial', description })
-}
+const compoundKey = (blockId: string, key: string) => `${blockId}/${key}`
 
-function useAvailableSources(contentTypeFilter?: string | ((contentType: string) => boolean)): Record<string, SourceDTO> {
-	const totalFields = useInternalStore((state) => state.totalFields)
-
-	return useMemo(() => {
-		const { mainStore, metaByHandle } = useInternalStore.getState()
-		return Object.entries(mainStore)
-			.flatMap(([handle, slice]) => {
-				let sources = Object.entries(slice).filter(([_, field]) => field.status !== 'unavailable')
-				if (contentTypeFilter) {
-					const filterByContentType = typeof contentTypeFilter === 'string' ? (ct: string) => ct.includes(contentTypeFilter) : contentTypeFilter
-					sources = sources.filter(([_, field]) => filterByContentType(field.contentType))
-				}
-
-				const { instanceName } = metaByHandle[handle]
-				return sources.map(([key, { status, value, ...rest }]) => ({ ...rest, key, slice: handle, instanceName }))
+const createBeacon =
+	(blockId: string) =>
+	(config: InitBeaconConfig): ProducerBeacon => {
+		return useMemo(() => {
+			const { key, ...rest } = config
+			useInternalStore.setState((state) => {
+				state.beacons[compoundKey(blockId, key)] = { ...rest, status: 'initial', value: rest.default }
 			})
-			.reduce((accumulator, source) => ({ ...accumulator, [`${source.slice}-${source.key}`]: source }), {})
-	}, [totalFields, contentTypeFilter])
-}
 
-function createDispatch(blockHandle: string, fieldKey: string) {
-	useEffect(() => {
+			return { key: compoundKey(blockId, key), default: rest.default, contentType: rest.contentType }
+		}, [])
+	}
+
+const revokeBeacon =
+	(_blockId: string) =>
+	({ key }: ProducerBeacon) => {
 		useInternalStore.setState((state) => {
-			if (!state.mainStore[blockHandle]) {
-				state.mainStore[blockHandle] = {}
-			}
-			if (!state.mainStore[blockHandle]?.[fieldKey]) {
-				state.mainStore[blockHandle][fieldKey] = { contentType: '', status: 'initial', description: '' }
+			if (state.beacons[key]) {
+				state.beacons[key].status = 'unavailable'
 			}
 		})
-	}, [blockHandle, fieldKey])
+	}
 
-	return (updateField: Partial<Omit<Field, 'isContentTypeDynamic'>>) => {
-		useInternalStore.setState((state: any) =>
-			Object.entries(updateField)
+function useLighthouse(config: InitBlockConfig): [(config: InitBeaconConfig) => ProducerBeacon, (config: ProducerBeacon) => void] {
+	const { blockId, ...rest } = config
+
+	useEffect(() => {
+		// do clean up on unmount
+		return () => {
+			useInternalStore.setState((state) => {
+				Object.entries(state.beacons)
+					.filter(([compoundKey, _]) => compoundKey.startsWith(blockId + '/'))
+					.forEach(([_, beacon]) => {
+						beacon.status = 'unavailable'
+					})
+			})
+		}
+	}, [])
+
+	useEffect(() => {
+		useInternalStore.setState((state) => {
+			state.blocks[blockId] = rest
+		})
+	}, [rest.instanceName])
+
+	return [createBeacon(blockId), revokeBeacon(blockId)]
+}
+
+function createDispatch(config: ProducerBeacon) {
+	const { key, contentType } = config
+	useEffect(() => {
+		useInternalStore.setState((state) => {
+			if (!state.beacons[key]) {
+				state.beacons[key] = { default: config.default, contentType, value: config.default, status: 'initial' }
+			}
+		})
+	}, [key])
+
+	return (update: Partial<BaseBeaconState>) => {
+		useInternalStore.setState((state) =>
+			Object.entries(update)
 				.filter(([_, itemVal]) => !!itemVal)
 				.forEach(([itemKey, itemVal]) => {
-					state.mainStore[blockHandle][fieldKey][itemKey] = itemVal
+					state.beacons[key][itemKey as keyof BaseBeaconState] = itemVal
 				})
 		)
 	}
 }
 
-function addFieldsCallback(blockHandle: string, fields: FieldWithKey[], state: Draft<StoreWrapper>) {
-	if (!state.mainStore[blockHandle]) {
-		state.mainStore[blockHandle] = {}
+function useAvailableBeacons(contentTypeFilter?: string | ((contentType: string) => boolean)): Record<string, ConsumerBeacon> {
+	const blocks = useInternalStore((state) => state.blocks)
+	const beacons = useInternalStore((state) => state.beacons)
+
+	return produce(beacons, (dictDraft: Draft<Record<string, BeaconState & ConsumerBeacon>>) => {
+		Object.keys(beacons).forEach((key) => {
+			const beacon: any = dictDraft[key]
+			let isExpectedContentType = true
+
+			if (contentTypeFilter) {
+				const compareContentType = typeof contentTypeFilter === 'string' ? (ct: string) => ct.includes(contentTypeFilter) : contentTypeFilter
+				isExpectedContentType = compareContentType(beacon.contentType)
+			}
+
+			if (beacon.status === 'unavailable' || !isExpectedContentType) {
+				delete dictDraft[key]
+			} else {
+				const blockId = key.split('/')[0]
+				const instanceName = blocks[blockId]?.instanceName ?? ''
+				beacon.description = `${instanceName}: ${beacon.description}`
+
+				beacon['key'] = key
+				delete beacon.error
+				delete beacon.value
+				delete beacon.status
+			}
+		})
+	}) as any
+}
+
+function useBeacon(config?: ConsumerBeacon): BaseBeaconState {
+	const { key, contentType, default: defaultVal } = config ?? { key: '', contentType: '' }
+	const state = useInternalStore((state) => state.beacons[key])
+
+	if (!state) {
+		return { contentType, value: defaultVal, status: 'initial' }
 	}
 
-	fields.forEach((field) => {
-		const { key, ...rest } = field
-		state.mainStore[blockHandle][key] = { ...rest, status: 'initial' }
-	})
-	state.totalFields += fields.length
-}
-
-function removeFieldsCallback(blockHandle: string, fields: string[], state: Draft<StoreWrapper>) {
-	fields.forEach((k) => {
-		state.mainStore[blockHandle][k].status = 'unavailable'
-	})
-	state.totalFields -= fields.length
-}
-
-function addBlock(blockName: string, handle: string, fields: FieldWithKey[]): string {
-	const blockHandle = handle ? handle : generateToken()
-	useInternalStore.setState((state) => {
-		state.metaByHandle[blockHandle] = {
-			blockType: blockName,
-			instanceName: blockName,
-		}
-		addFieldsCallback(blockHandle, fields, state)
-	})
-
-	return blockHandle
-}
-
-function removeBlock(blockHandle: string) {
-	useInternalStore.setState((state) => {
-		const slice = state.mainStore[blockHandle]
-		const fields = Object.keys(slice)
-		removeFieldsCallback(blockHandle, fields, state)
-	})
-}
-
-function addField(blockHandle: string, field: FieldWithKey) {
-	useInternalStore.setState((state) => addFieldsCallback(blockHandle, [field], state))
-}
-
-function removeField(blockHandle: string, key: string) {
-	useInternalStore.setState((state) => removeFieldsCallback(blockHandle, [key], state))
-}
-
-function setInstanceName(blockHandle: string, instanceName: string) {
-	useInternalStore.setState((state) => {
-		if (state.metaByHandle[blockHandle]) {
-			state.metaByHandle[blockHandle].instanceName = instanceName
-		}
-	})
+	return state
 }
 
 export default {
-	useAvailableSources,
-	useInseriStore,
+	useLighthouse,
 	createDispatch,
-	setInstanceName,
-	addBlock,
-	addField,
-	removeBlock,
-	removeField,
+	useAvailableBeacons,
+	useBeacon,
 } as const
