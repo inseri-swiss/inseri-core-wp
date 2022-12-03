b1f586e99b999b0795534f499c408596dfd1ca1a
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2022-12-03 17:51:48 +0100

    feat: init blockName earlier

--- src/components/SetupEditorEnv.tsx
+++ src/components/SetupEditorEnv.tsx
@@ -5,14 +5,18 @@ import type { PropsWithChildren } from 'react'
 import { generateId } from '@inseri/utils'
 import { InseriThemeProvider } from './InseriThemeProvider'
 
-export function SetupEditorEnv(props: PropsWithChildren<BlockEditProps<any>> & { blockIdName?: string }) {
-	const { setAttributes, attributes, blockIdName = 'blockId', children } = props
+export function SetupEditorEnv(props: PropsWithChildren<BlockEditProps<any>> & { baseBlockName: string }) {
+	const { setAttributes, attributes, children, baseBlockName } = props
 
 	useEffect(() => {
-		if (!attributes[blockIdName]) {
-			setAttributes({ [blockIdName]: generateId() })
+		if (!attributes.blockId) {
+			setAttributes({ blockId: generateId() })
+		}
+
+		if (!attributes.blockName) {
+			setAttributes({ blockName: `${baseBlockName}-${generateId(3)}` })
 		}
 	}, [])
 
-	return <div {...useBlockProps()}>{attributes[blockIdName] ? <InseriThemeProvider>{children}</InseriThemeProvider> : null}</div>
+	return <div {...useBlockProps()}>{attributes.blockId ? <InseriThemeProvider>{children}</InseriThemeProvider> : null}</div>
 }
