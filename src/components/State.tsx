a60be0929e9f46e69e93d468197d51d3f8bbfdbe
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2022-12-22 14:32:21 +0100

    feat: use immer, devtools middlewares

--- src/components/State.tsx
+++ src/components/State.tsx
@@ -2,30 +2,49 @@ import { createContext, useContext, useRef } from '@wordpress/element'
 import type { PropsWithChildren } from 'react'
 import { persistToAttributes } from '../utils'
 import create, { useStore, StateCreator, StoreApi } from 'zustand'
+import { devtools } from 'zustand/middleware'
+import { immer } from 'zustand/middleware/immer'
+import { setAutoFreeze } from 'immer'
 
 const StateContext = createContext<StoreApi<any> | undefined>(undefined)
 
-interface StateProviderProps<T> extends PropsWithChildren<any> {
-	stateCreator: (initialState?: Partial<T>) => StateCreator<T, [], []>
-
-	setAttributes?: (attrs: Partial<Record<string, any>>) => void
-	attributes?: Readonly<Record<string, any>>
-	keysInSync?: string[]
+interface SimpleProps<T> extends PropsWithChildren<any> {
+	stateCreator: (initialState: T) => StateCreator<T, [], []>
+	initialState: T
+}
+interface AttributesProps<T> extends PropsWithChildren<any> {
+	stateCreator: (initialState: T) => StateCreator<T, [], []>
+	setAttributes: (attrs: Partial<Record<string, any>>) => void
+	attributes: Readonly<Record<string, any>>
+	keysInSync: string[]
 }
 
-export function StateProvider<T>({ children, stateCreator, setAttributes, attributes, keysInSync }: StateProviderProps<T>) {
+type StateProviderProps<T> = SimpleProps<T> | AttributesProps<T>
+
+export function StateProvider<T>({ initialState, children, stateCreator, setAttributes, attributes, keysInSync }: StateProviderProps<T>) {
 	const storeRef = useRef<StoreApi<any>>()
 
 	if (!storeRef.current) {
+		let store: any
+
 		if (setAttributes && attributes && keysInSync) {
 			const initialStateFromAttributes = Object.entries(attributes)
 				.filter(([k, _]) => keysInSync.includes(k))
-				.reduce((acc, [k, v]) => ({ ...acc, [k]: v }), {})
+				.reduce((acc, [k, v]) => ({ ...acc, [k]: v }), {}) as T
 
-			storeRef.current = create(persistToAttributes(stateCreator(initialStateFromAttributes) as any, { setAttributes, keysToSave: keysInSync }))
+			store = persistToAttributes(stateCreator(initialStateFromAttributes) as any, { setAttributes, keysToSave: keysInSync })
 		} else {
-			storeRef.current = create(stateCreator())
+			store = stateCreator(initialState)
 		}
+
+		if (process.env.NODE_ENV !== 'production') {
+			store = devtools(store, { name: attributes.blockId ?? 'component' })
+			setAutoFreeze(true)
+		} else {
+			setAutoFreeze(false)
+		}
+
+		storeRef.current = create(immer(store))
 	}
 
 	return <StateContext.Provider value={storeRef.current}>{children}</StateContext.Provider>
