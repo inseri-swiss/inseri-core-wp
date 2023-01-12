c1cfc722d7dc9b46e0c7c08adf8ec9d188b1b1ab
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2023-01-12 12:28:47 +0100

    feat: enable also to consume plain urls

--- src/globalScript/utils.ts
+++ src/globalScript/utils.ts
@@ -1,4 +1,5 @@
 import Ajv, { Schema } from 'ajv'
+import addFormats from 'ajv-formats'
 import produce from 'immer'
 import { customAlphabet } from 'nanoid/non-secure'
 
@@ -7,6 +8,7 @@ const nanoid = customAlphabet(alphabet, 21)
 const generateId = (size?: number) => nanoid(size)
 
 const ajv = new Ajv({ allErrors: true })
+addFormats(ajv)
 
 function initJsonValidator<T = any>(schema: Schema) {
 	return ajv.compile<T>(schema)
