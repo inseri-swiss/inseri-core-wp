fb30b35c3b7c50b36d823e23951d42babd25a38b
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2023-01-12 11:29:01 +0100

    feat: scaffold image block

---
+++ src/blocks/image/index.tsx
@@ -0,0 +1,46 @@
+import { ConsumerBeacon } from '@inseri/lighthouse'
+import { IconPhoto } from '@tabler/icons'
+import { useBlockProps } from '@wordpress/block-editor'
+import type { BlockEditProps, BlockSaveProps } from '@wordpress/blocks'
+import { registerBlockType } from '@wordpress/blocks'
+import stringify from 'json-stable-stringify'
+import { SetupEditorEnv, StateProvider } from '../../components'
+import json from './block.json'
+import { PhotoEdit } from './Component'
+import { storeCreator } from './state'
+
+const { name, ...settings } = json as any
+
+export interface Attributes {
+	blockId: string
+	blockName: string
+	input: ConsumerBeacon
+	caption: string
+	altText: string
+}
+
+function Edit(props: BlockEditProps<Attributes>) {
+	const { setAttributes, attributes } = props
+	return (
+		<SetupEditorEnv {...props} baseBlockName={'image'}>
+			<StateProvider stateCreator={storeCreator} keysToSave={Object.keys(json.attributes)} setAttributes={setAttributes} initialState={attributes}>
+				<PhotoEdit {...props} />
+			</StateProvider>
+		</SetupEditorEnv>
+	)
+}
+
+function Save({ attributes }: BlockSaveProps<Attributes>) {
+	return (
+		<div {...useBlockProps.save()} data-attributes={stringify(attributes)}>
+			is loading ...
+		</div>
+	)
+}
+
+registerBlockType<Attributes>(name, {
+	...settings,
+	edit: Edit,
+	save: Save,
+	icon: <IconPhoto style={{ fill: 'none' }} />,
+})
