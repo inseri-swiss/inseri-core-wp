6f29910d6b7439773c1fa64fb2b25571e88838a4
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2023-03-07 14:46:37 +0100

    feat: init html block

---
+++ src/blocks/html/Component.tsx
@@ -0,0 +1,98 @@
+import { useAvailableBeacons, useWatch } from '@inseri/lighthouse'
+import { IconBrandHtml5 } from '@tabler/icons'
+import { BlockControls, InspectorControls } from '@wordpress/block-editor'
+import type { BlockEditProps } from '@wordpress/blocks'
+import { PanelBody, PanelRow, TextControl, ToolbarButton, ToolbarGroup } from '@wordpress/components'
+import { useEffect } from '@wordpress/element'
+import { __ } from '@wordpress/i18n'
+import { edit } from '@wordpress/icons'
+import { Box, CodeEditor, Group, Select, Text, useGlobalState } from '../../components'
+import { Attributes } from './index'
+import { GlobalState } from './state'
+
+export function HtmlEdit(props: BlockEditProps<Attributes>) {
+	const { isSelected } = props
+
+	const { input, blockId, blockName, isWizardMode, actions } = useGlobalState((state: GlobalState) => state)
+	const isValueSet = !!input.key
+	const inputBeaconKey = input.key
+
+	const { updateState, chooseInputBeacon } = actions
+
+	const availableBeacons = useAvailableBeacons('html')
+	const selectData = Object.keys(availableBeacons)
+		.filter((k) => !k.startsWith(blockId + '/'))
+		.map((k) => ({ label: availableBeacons[k].description, value: k }))
+
+	const { status } = useWatch(input)
+	useEffect(() => {
+		if (status === 'unavailable') {
+			updateState({ input: { ...input, key: '' }, isWizardMode: true })
+		}
+	}, [status])
+
+	useEffect(() => {
+		if (isValueSet && !isSelected && isWizardMode) {
+			updateState({ isWizardMode: false })
+		}
+	}, [isSelected])
+
+	return (
+		<>
+			<BlockControls>
+				{isValueSet && (
+					<ToolbarGroup>
+						<ToolbarButton icon={edit} title={__('Edit', 'inseri-core')} isActive={isWizardMode} onClick={() => updateState({ isWizardMode: !isWizardMode })} />
+					</ToolbarGroup>
+				)}
+			</BlockControls>
+			<InspectorControls key="setting">
+				<PanelBody>
+					<PanelRow>
+						<TextControl label="Block Name" value={blockName} onChange={(value) => updateState({ blockName: value })} />
+					</PanelRow>
+				</PanelBody>
+			</InspectorControls>
+			{isWizardMode ? (
+				<Box p="md" style={{ border: '1px solid #000' }}>
+					<Group mb="md" spacing={0}>
+						<IconBrandHtml5 size={28} />
+						<Text ml="xs" fz={24}>
+							{__('HTML Code', 'inseri-core')}
+						</Text>
+					</Group>
+
+					<Select
+						label={__('Render HTML by selecting a block source', 'inseri-core')}
+						data={selectData}
+						value={inputBeaconKey}
+						onChange={(key) => chooseInputBeacon(availableBeacons[key!])}
+					/>
+				</Box>
+			) : (
+				<HtmlView />
+			)}
+		</>
+	)
+}
+
+interface ViewProps {
+	isGutenbergEditor?: boolean
+}
+
+export function HtmlView({}: ViewProps) {
+	const { height, input } = useGlobalState((state: GlobalState) => state)
+	const { value, status } = useWatch(input)
+
+	let preparedValue = value
+
+	if ((status !== 'ready' && status !== 'initial') || !preparedValue) {
+		preparedValue = ''
+	}
+
+	return (
+		<Box p="md">
+			<CodeEditor height={height} type={'html'} value={preparedValue} />
+		</Box>
+	)
+}
---
+++ src/blocks/html/index.tsx
@@ -0,0 +1,45 @@
+import { IconBrandHtml5 } from '@tabler/icons'
+import { useBlockProps } from '@wordpress/block-editor'
+import type { BlockEditProps, BlockSaveProps } from '@wordpress/blocks'
+import { registerBlockType } from '@wordpress/blocks'
+import stringify from 'json-stable-stringify'
+import { SetupEditorEnv, StateProvider } from '../../components'
+import { ConsumerBeacon } from '../../globalScript'
+import json from './block.json'
+import { HtmlEdit } from './Component'
+import { storeCreator } from './state'
+import './style.scss'
+
+const { name, ...settings } = json as any
+
+export interface Attributes {
+	blockId?: string
+	blockName: string
+	input: ConsumerBeacon
+}
+
+function Edit(props: BlockEditProps<Attributes>) {
+	const { setAttributes, attributes } = props
+	return (
+		<SetupEditorEnv {...props} baseBlockName={'textEditor'}>
+			<StateProvider stateCreator={storeCreator} keysToSave={Object.keys(json.attributes)} setAttributes={setAttributes} initialState={attributes}>
+				<HtmlEdit {...props} />
+			</StateProvider>
+		</SetupEditorEnv>
+	)
+}
+
+function Save({ attributes }: BlockSaveProps<Attributes>) {
+	return (
+		<div {...useBlockProps.save()} data-attributes={stringify(attributes)}>
+			is loading ...
+		</div>
+	)
+}
+
+registerBlockType<Attributes>(name, {
+	...settings,
+	edit: Edit,
+	save: Save,
+	icon: <IconBrandHtml5 style={{ fill: 'none' }} />,
+})
