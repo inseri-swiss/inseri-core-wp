512cb77874b3bfba251ae9bbfde8f934155b0659
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2023-03-12 11:39:01 +0100

    feat: scaffold plotly block

---
+++ src/blocks/plotly/state.tsx
@@ -0,0 +1,37 @@
+import { immer } from 'zustand/middleware/immer'
+import { updatePartially } from '../../utils'
+import { Attributes } from './index'
+
+export interface GlobalState extends Attributes {
+	[i: string]: any
+
+	isWizardMode: boolean
+	isBlob: boolean
+	isPrevBlob: boolean
+	imageUrl: string
+	prevImageUrl: string
+	hasError: boolean
+
+	actions: {
+		updateState: (modifier: Partial<GlobalState>) => void
+	}
+}
+
+export const storeCreator = (initalState: Attributes) => {
+	return immer<GlobalState>((set) => ({
+		...initalState,
+		isWizardMode: !initalState.inputData.key,
+		isBlob: false,
+		isPrevBlob: false,
+		imageUrl: '',
+		prevImageUrl: '',
+		hasError: false,
+
+		actions: {
+			updateState: (modifier: RecursivePartial<GlobalState>) =>
+				set((state) => {
+					updatePartially(state, modifier)
+				}),
+		},
+	}))
+}
