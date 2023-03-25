f8fb89fd7e8285eef4559cffe2b1453ad5d1a244
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2023-03-25 14:44:09 +0100

    feat: scaffold download block

---
+++ src/blocks/download/state.tsx
@@ -0,0 +1,27 @@
+import { immer } from 'zustand/middleware/immer'
+import { updatePartially } from '../../utils'
+import { Attributes } from './index'
+
+export interface GlobalState extends Attributes {
+	[i: string]: any
+
+	isWizardMode: boolean
+
+	actions: {
+		updateState: (modifier: Partial<GlobalState>) => void
+	}
+}
+
+export const storeCreator = (initalState: Attributes) => {
+	return immer<GlobalState>((set) => ({
+		...initalState,
+		isWizardMode: !initalState.input.key,
+
+		actions: {
+			updateState: (modifier: RecursivePartial<GlobalState>) =>
+				set((state) => {
+					updatePartially(state, modifier)
+				}),
+		},
+	}))
+}
