5625a79e1d1c1da8d8573eb4f6519a18e5453951
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2023-08-02 15:43:39 +0200

    feat: migrate download state to recoil

--- src/blocks/download/edit.tsx
+++ src/blocks/download/edit.tsx
@@ -6,22 +6,27 @@ import { PanelBody, PanelRow, TextControl, ToolbarButton, ToolbarGroup } from '@
 import { useEffect } from '@wordpress/element'
 import { __ } from '@wordpress/i18n'
 import { edit } from '@wordpress/icons'
-import { Box, Group, Select, SetupEditorEnv, StateProvider, Text, useGlobalState } from '../../components'
+import { RecoilRoot, useRecoilState } from 'recoil'
+import { Box, Group, Select, SetupEditorEnv, Text } from '../../components'
 import json from './block.json'
 import { Attributes } from './index'
-import { GlobalState, storeCreator } from './state'
+import { wizardState } from './state'
 import View from './view'
 
 function EditComponent(props: BlockEditProps<Attributes>) {
-	const { isSelected } = props
-	const { blockName, inputKey, isWizardMode, actions, label, fileName } = useGlobalState((state: GlobalState) => state)
-	const { updateState } = actions
+	const { isSelected, setAttributes, attributes } = props
+	const { blockId, blockName, inputKey, label, fileName } = attributes
 
+	const [isWizardMode, setWizardMode] = useRecoilState(wizardState(blockId))
 	const isValueSet = !!inputKey
 
 	useEffect(() => {
+		setWizardMode(!inputKey)
+	}, [])
+
+	useEffect(() => {
 		if (isValueSet && !isSelected && isWizardMode) {
-			updateState({ isWizardMode: false })
+			setWizardMode(false)
 		}
 	}, [isSelected])
 
@@ -33,27 +38,20 @@ function EditComponent(props: BlockEditProps<Attributes>) {
 			<BlockControls>
 				{isValueSet && (
 					<ToolbarGroup>
-						<ToolbarButton
-							icon={edit}
-							isActive={isWizardMode}
-							onClick={() => {
-								updateState({ isWizardMode: !isWizardMode })
-							}}
-							title={__('Edit', 'inseri-core')}
-						/>
+						<ToolbarButton icon={edit} isActive={isWizardMode} onClick={() => setWizardMode(!isWizardMode)} title={__('Edit', 'inseri-core')} />
 					</ToolbarGroup>
 				)}
 			</BlockControls>
 			<InspectorControls key="setting">
 				<PanelBody>
 					<PanelRow>
-						<TextControl label={__('Block Name', 'inseri-core')} value={blockName} onChange={(value) => updateState({ blockName: value })} />
+						<TextControl label={__('Block Name', 'inseri-core')} value={blockName} onChange={(value) => setAttributes({ blockName: value })} />
 					</PanelRow>
 					<PanelRow>
-						<TextControl label={__('Label', 'inseri-core')} value={label} onChange={(value) => updateState({ label: value })} />
+						<TextControl label={__('Label', 'inseri-core')} value={label} onChange={(value) => setAttributes({ label: value })} />
 					</PanelRow>
 					<PanelRow>
-						<TextControl label={__('File Name', 'inseri-core')} value={fileName} onChange={(value) => updateState({ fileName: value })} />
+						<TextControl label={__('File Name', 'inseri-core')} value={fileName} onChange={(value) => setAttributes({ fileName: value })} />
 					</PanelRow>
 				</PanelBody>
 			</InspectorControls>
@@ -70,25 +68,28 @@ function EditComponent(props: BlockEditProps<Attributes>) {
 						data={options}
 						value={inputKey}
 						searchable
-						onChange={(key) => updateState({ inputKey: key ?? '', isWizardMode: false })}
+						onChange={(key) => {
+							setAttributes({ inputKey: key ?? '' })
+							setWizardMode(false)
+						}}
 					/>
 				</Box>
 			) : (
-				<View />
+				<View attributes={attributes} setAttributes={setAttributes} />
 			)}
 		</>
 	)
 }
 
 export default function Edit(props: BlockEditProps<Attributes>) {
-	const { setAttributes, attributes } = props
+	const { attributes } = props
 	return (
 		<SetupEditorEnv {...props} baseBlockName={'download'}>
-			<StateProvider stateCreator={storeCreator} keysToSave={Object.keys(json.attributes)} setAttributes={setAttributes} initialState={attributes}>
+			<RecoilRoot>
 				<InseriRoot blockId={attributes.blockId} blockName={attributes.blockName} blockType={json.name}>
 					<EditComponent {...props} />
 				</InseriRoot>
-			</StateProvider>
+			</RecoilRoot>
 		</SetupEditorEnv>
 	)
 }
