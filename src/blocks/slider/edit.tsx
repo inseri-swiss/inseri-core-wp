86990fe282b169832834ab50348881445a74295f
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2023-12-19 19:25:47 +0100

    feat: init slider block

---
+++ src/blocks/slider/edit.tsx
@@ -0,0 +1,49 @@
+import { InseriRoot } from '@inseri/lighthouse'
+import { InspectorControls } from '@wordpress/block-editor'
+import type { BlockEditProps } from '@wordpress/blocks'
+import { PanelBody, PanelRow } from '@wordpress/components'
+import { SetupEditorEnv, StateProvider, TextInput, useGlobalState } from '../../components'
+import config from './block.json'
+import { Attributes } from './index'
+import { GlobalState, storeCreator } from './state'
+import View from './view'
+
+function EditComponent(_props: BlockEditProps<Attributes>) {
+	const { label, blockName, actions } = useGlobalState((state: GlobalState) => state)
+	const { updateState } = actions
+
+	return (
+		<>
+			<InspectorControls key="setting">
+				<PanelBody>
+					<PanelRow>
+						<TextInput
+							styles={{ root: { width: '100%' } }}
+							label="Block Name"
+							value={blockName}
+							onChange={(event) => updateState({ blockName: event.target.value })}
+						/>
+					</PanelRow>
+					<PanelRow>
+						<TextInput styles={{ root: { width: '100%' } }} label="Label" value={label} onChange={(event) => updateState({ label: event.target.value })} />
+					</PanelRow>
+				</PanelBody>
+			</InspectorControls>
+
+			<View />
+		</>
+	)
+}
+
+export default function Edit(props: BlockEditProps<Attributes>) {
+	const { setAttributes, attributes, clientId } = props
+	return (
+		<SetupEditorEnv {...props} baseBlockName={'slider'}>
+			<StateProvider stateCreator={storeCreator} keysToSave={Object.keys(config.attributes)} setAttributes={setAttributes} initialState={attributes}>
+				<InseriRoot blockId={attributes.blockId} blockName={attributes.blockName} blockType={config.name} clientId={clientId}>
+					<EditComponent {...props} />
+				</InseriRoot>
+			</StateProvider>
+		</SetupEditorEnv>
+	)
+}
