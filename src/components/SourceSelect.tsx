cd5eb28373f93f6b27ae9b12530e4f275d69b1ee
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2024-03-19 18:58:48 +0100

    feat: migrate cytoscape to new select

--- src/components/SourceSelect.tsx
+++ src/components/SourceSelect.tsx
@@ -48,56 +48,63 @@ const SelectItem = forwardRef<HTMLDivElement, ItemProps>((props: ItemProps, ref)
 })
 
 interface Props {
+	label?: string
 	data: DiscoveredItem[]
 	selectValue: string | null
 	activeTab: string | null
+	tabs: string[]
 	onSelectChange(value: string | null): void
 	setActiveTab(value: string | null): void
+	style?: React.CSSProperties
 }
 
-export function SourceSelect({ data, selectValue, activeTab, onSelectChange, setActiveTab }: Props) {
+export function SourceSelect({ label, data, selectValue, activeTab, tabs, onSelectChange, setActiveTab, style }: Props) {
 	const { classes } = useStyles()
 	const { ref, focused } = useFocusWithin()
 
 	return (
-		<Box style={{ border: focused ? '1px solid #1971c2' : '1px solid #8c8f94', borderRadius: '4px' }}>
-			<Tabs value={activeTab} onTabChange={setActiveTab} styles={{ tab: { borderWidth: '4px' } }}>
-				<Tabs.List>
-					<Tabs.Tab value="all">All</Tabs.Tab>
-					<Tabs.Tab value="textual">Textual</Tabs.Tab>
-				</Tabs.List>
+		<div style={style}>
+			{label && <Text fz="sm">{label}</Text>}
+			<Box style={{ border: focused ? '1px solid #1971c2' : '1px solid #8c8f94', borderRadius: '4px' }}>
+				<Tabs value={activeTab} onTabChange={setActiveTab} styles={{ tab: { borderWidth: '4px' } }}>
+					<Tabs.List>
+						{tabs.map((t) => (
+							<Tabs.Tab value={t.replaceAll(' ', '-')}>{t}</Tabs.Tab>
+						))}
+					</Tabs.List>
 
-				<Select
-					ref={ref}
-					data={data}
-					value={selectValue}
-					itemComponent={SelectItem}
-					clearable
-					searchable
-					onChange={onSelectChange}
-					placeholder="Search for blocks, content type, ..."
-					classNames={{ wrapper: classes.inputWrapper }}
-					styles={{
-						item: {
-							padding: '1.25rem 0.25rem',
-							border: '0',
-							borderBottom: '1px solid #e0e0e0',
-						},
-					}}
-					filter={(value, item) => {
-						const { label = '', blockName = '', contentType = '', blockTitle = '' } = item
-						const contentTypeDescription = COMMON_CONTENT_TYPES.find((c) => c.value === contentType)?.label ?? contentType
-						const searchValue = value.trim()
+					<Select
+						ref={ref}
+						data={data}
+						value={selectValue}
+						itemComponent={SelectItem}
+						clearable
+						searchable
+						onChange={onSelectChange}
+						placeholder="Search for blocks, content type, ..."
+						classNames={{ wrapper: classes.inputWrapper }}
+						styles={{
+							item: {
+								padding: '1.25rem 0.25rem',
+								border: '0',
+								borderBottom: '1px solid #e0e0e0',
+							},
+						}}
+						filter={(value, item) => {
+							const { label = '', blockName = '', contentType = '', blockTitle = '' } = item
+							const contentTypeDescription = COMMON_CONTENT_TYPES.find((c) => c.value === contentType)?.label ?? contentType
+							const searchValue = value.trim()
 
-						return (
-							label.toLowerCase().includes(searchValue) ||
-							blockName.toLowerCase().includes(searchValue) ||
-							contentTypeDescription.toLowerCase().includes(searchValue) ||
-							blockTitle.toLowerCase().includes(searchValue)
-						)
-					}}
-				/>
-			</Tabs>
-		</Box>
+							return (
+								label.toLowerCase().includes(searchValue) ||
+								blockName.toLowerCase().includes(searchValue) ||
+								contentTypeDescription.toLowerCase().includes(searchValue) ||
+								blockTitle.toLowerCase().includes(searchValue)
+							)
+						}}
+					/>
+				</Tabs>
+			</Box>
+		</div>
 	)
 }
