6b5128fcc1915f854c54f39f69229c92a1087ef6
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2024-04-02 15:49:50 +0200

    feat: generate querySelector to make future-proof

--- src/blocks/download/hydration.tsx
+++ src/blocks/download/hydration.tsx
@@ -3,11 +3,12 @@ import domReady from '@wordpress/dom-ready'
 import { createRoot } from '@wordpress/element'
 import { RecoilRoot } from 'recoil'
 import { InseriThemeProvider } from '../../components'
+import { generateQuerySelector } from '../../utils'
 import config from './block.json'
 import View from './view'
 
 function initReactComponents() {
-	const items = document.querySelectorAll('.wp-block-inseri-core-download')
+	const items = document.querySelectorAll(generateQuerySelector(config.name))
 	if (items) {
 		Array.from(items).forEach((item: any) => {
 			const attributes = JSON.parse(item.dataset.attributes)
--- src/blocks/localFileImport/hydration.tsx
+++ src/blocks/localFileImport/hydration.tsx
@@ -1,13 +1,14 @@
 import { InseriRoot } from '@inseri/lighthouse'
 import domReady from '@wordpress/dom-ready'
 import { createRoot } from '@wordpress/element'
+import { generateQuerySelector } from '../../utils'
 import { InseriThemeProvider, StateProvider } from '../../components'
+import config from './block.json'
 import { storeCreator } from './state'
 import View from './view'
-import config from './block.json'
 
 function initReactComponents() {
-	const items = document.querySelectorAll('.wp-block-inseri-core-file-drop')
+	const items = document.querySelectorAll(generateQuerySelector(config.name))
 	if (items) {
 		Array.from(items).forEach((item: any) => {
 			const attributes = JSON.parse(item.dataset.attributes)
