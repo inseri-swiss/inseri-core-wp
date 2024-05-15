9a0a25afc62a62edb6893d51364ded5451dfed2d
Arooran Thanabalasingam <13069024+bigaru@users.noreply.github.com>
2024-05-15 12:28:57 +0200

    feat: handle srcset of img tag

--- src/blocks/staticAsset/view.tsx
+++ src/blocks/staticAsset/view.tsx
@@ -26,6 +26,7 @@ const forEachArray = (attribute: string, assets: any[]) => (element: Element) =>
 }
 
 const make = (setCallback: (url: string) => void) => async () => {
+	const host = window.location.origin
 	const url = window.location.toString()
 
 	const response = await fetch(url)
@@ -38,6 +39,35 @@ const make = (setCallback: (url: string) => void) => async () => {
 	doc.querySelectorAll('script').forEach(forEachArray('src', assets))
 	doc.querySelectorAll('img').forEach(forEachArray('src', assets))
 	doc.querySelectorAll('link[rel="stylesheet"]').forEach(forEachArray('href', assets))
+
+	doc.querySelectorAll('img').forEach((element) => {
+		const srcSet =
+			element
+				.getAttribute('srcset')
+				?.split(',')
+				.map((e) => e.trim().split(' ') as [string, string]) ?? []
+
+		srcSet.forEach(([url, _]) => {
+			if (url && url.startsWith(host)) {
+				assets.push(url)
+			}
+		})
+
+		if (srcSet.length > 1 && srcSet.some(([url, _]) => url.startsWith(host))) {
+			const newSrcSet = srcSet
+				.map(([url, density]) => {
+					if (url.startsWith(host)) {
+						const splits = url.split('/')
+						const newUrl = `assets/${encodeURIComponent(splits[splits.length - 1])}`
+						return newUrl + ' ' + density
+					}
+					return url + ' ' + density
+				})
+				.join(', ')
+
+			element.setAttribute('srcset', newSrcSet)
+		}
+	})
 
 	const htmlText = new XMLSerializer().serializeToString(doc)
 	const htmlBlob = new Blob([htmlText])
