adc01c51082d134aa62a757539f708b8ef014b0a
Arooran Thanabalasingam <13069024+bigaru@users.noreply.github.com>
2024-05-28 15:58:09 +0200

    feat: init export block

---
+++ src/blocks/export/index.tsx
@@ -0,0 +1,27 @@
+import { IconPackageExport } from '@tabler/icons-react'
+import { useBlockProps } from '@wordpress/block-editor'
+import type { BlockSaveProps } from '@wordpress/blocks'
+import { registerBlockType } from '@wordpress/blocks'
+import stringify from 'json-stable-stringify'
+import json from './block.json'
+import Edit from './edit'
+
+const { name, ...settings } = json as any
+
+export interface Attributes {
+	blockId: string
+	blockName: string
+}
+
+registerBlockType<Attributes>(name, {
+	...settings,
+	edit: Edit,
+	save: ({ attributes }: BlockSaveProps<Attributes>) => {
+		return (
+			<div {...useBlockProps.save()} data-attributes={stringify(attributes)}>
+				is loading ...
+			</div>
+		)
+	},
+	icon: <IconPackageExport style={{ fill: 'none' }} />,
+})
