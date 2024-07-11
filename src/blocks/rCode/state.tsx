475953878ea0c4a90622dfbbc44d84abed5b6b7c
Arooran Thanabalasingam <13069024+bigaru@users.noreply.github.com>
2024-07-11 16:39:43 +0200

    feat: handle input and output

--- src/blocks/rCode/state.tsx
+++ src/blocks/rCode/state.tsx
@@ -1,35 +1,57 @@
-import type { Draft } from 'immer'
 import { immer } from 'zustand/middleware/immer'
-import { Action, CommonCodeState } from '../../components'
+import { CommonCodeState } from '../../components'
 import { Attributes } from './index'
+import { WebR, REnvironment, isRDouble, isRCharacter, isRList, isRInteger, isRRaw, isRLogical, isRComplex, isRSymbol, isRPairlist } from 'webr'
 
-export interface GlobalState extends Attributes, CommonCodeState {}
-
-const createWorker = (set: (nextStateOrUpdater: (state: Draft<GlobalState>) => void) => void) => {
-	const worker = new Worker(new URL(inseriApiSettings.pyWorker))
-
-	worker.addEventListener('message', ({ data }: MessageEvent<Action>) => {
-		set((state) => {
-			if (data.type === 'STATUS') {
-				state.workerStatus = data.payload
-			}
-			if (data.type === 'SET_STD_STREAM') {
-				state.stdStream = data.payload
-			}
-		})
-	})
-
-	return worker
+export interface GlobalState extends Attributes, CommonCodeState {
+	worker: any
+	webR: WebR
+	outputRecord: Record<string, any>
+	outputRevision: number
 }
+
+const convertRObject =
+	(env: REnvironment) =>
+	async (name: string): Promise<[string, any]> => {
+		const rVariable = await env.get(name)
+		let jsVariable: any = undefined
+
+		if (isRDouble(rVariable) || isRInteger(rVariable) || isRRaw(rVariable)) {
+			jsVariable = await rVariable.toNumber()
+		} else if (isRCharacter(rVariable) || isRSymbol(rVariable)) {
+			jsVariable = await rVariable.toString()
+		} else if (isRLogical(rVariable)) {
+			jsVariable = await rVariable.toBoolean()
+		} else if (isRComplex(rVariable)) {
+			jsVariable = await rVariable.toComplex()
+		} else if (isRList(rVariable) || isRPairlist(rVariable)) {
+			jsVariable = await rVariable.toArray()
+		} else {
+			jsVariable = await rVariable.toJs({ depth: 0 /*  infinite */ })
+		}
+
+		return [name, jsVariable]
+	}
 
 export const storeCreator = (initalState: Attributes) => {
 	const isValueSet = initalState.mode === 'editor' || (!!initalState.mode && !!initalState.inputCode)
+	const webR = new WebR({ interactive: false, channelType: 3 })
 
 	return immer<GlobalState>((set, get) => {
+		webR.init().then(() => {
+			set((state) => {
+				state.workerStatus = 'ready'
+			})
+		})
+
 		return {
 			...initalState,
 
-			worker: createWorker(set),
+			webR,
+			outputRecord: {},
+			outputRevision: 0,
+
+			worker: undefined,
 			workerStatus: 'initial',
 			stdStream: '',
 			inputerr: '',
@@ -74,20 +96,56 @@ export const storeCreator = (initalState: Attributes) => {
 					})
 				},
 
-				runCode: (code: string) => {
+				runCode: async (code: string) => {
 					set((state) => {
 						state.stdStream = ''
+						state.workerStatus = 'in-progress'
 					})
 
-					const { worker } = get()
-					worker.postMessage({ type: 'RUN_CODE', payload: code })
+					const { webR, inputRecord, outputs } = get()
+
+					let outputRecord = {}
+					let env
+
+					try {
+						env = await new webR.REnvironment(inputRecord)
+					} catch (error) {
+						const emptyRecord = Object.keys(inputRecord).reduce((acc, key) => ({ ...acc, [key]: null }), {})
+						env = await new webR.REnvironment(emptyRecord)
+
+						if (error instanceof Error) {
+							let message = error.message
+							set((state) => {
+								state.stdStream = message
+							})
+						}
+					}
+
+					const shelter = await new webR.Shelter()
+
+					try {
+						const capture = await shelter.captureR(code, { env })
+						const converter = convertRObject(env)
+						outputRecord = (await Promise.all(outputs.map((o) => converter(o[0])))).reduce((acc, [name, val]) => {
+							acc[name] = val
+							return acc
+						}, {} as any)
+					} finally {
+						shelter.purge()
+					}
+
+					set((state) => {
+						state.workerStatus = 'ready'
+						state.outputRecord = outputRecord
+						state.outputRevision++
+					})
 				},
 
 				terminate: () => {
 					get().worker.terminate()
 					set((state) => {
 						state.workerStatus = 'initial'
-						state.worker = createWorker(set)
+						//state.worker = createWorker(set)
 					})
 				},
 
