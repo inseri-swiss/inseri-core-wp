c5c297b2970b4cebc56bd9a83ba662f13c7bd263
Arooran Thanabalasingam <13069024+bigaru@users.noreply.github.com>
2024-08-08 10:55:09 +0200

    feat: migrate the corresponding attributes

--- src/components/deprecation.tsx
+++ src/components/deprecation.tsx
@@ -1,24 +1,34 @@
 import { useBlockProps } from '@wordpress/block-editor'
 import stringify from 'json-stable-stringify'
 
-export const deprecateBlockName = (settings: { attributes: any; supports: any }) => ({
-	attributes: settings.attributes,
-	supports: settings.supports,
-	save: ({ attributes }: any) => {
-		return (
-			<div {...useBlockProps.save()} data-attributes={stringify(attributes)}>
-				is loading ...
-			</div>
-		)
-	},
-	migrate: (attributes: { blockName: string }) => {
-		const { blockName, ...rest } = attributes
-		return {
-			...rest,
-			metadata: {
-				name: blockName,
+export const deprecateBlockName = (settings: { attributes: any; supports: any }) => {
+	const { metadata, ...oldAttributes } = settings.attributes
+
+	return {
+		attributes: {
+			...oldAttributes,
+			blockName: {
+				type: 'string',
+				default: '',
 			},
-		}
-	},
-	isEligible: (attributes: { blockName: string }) => typeof attributes.blockName === 'string',
-})
+		},
+		supports: settings.supports,
+		save: ({ attributes }: any) => {
+			return (
+				<div {...useBlockProps.save()} data-attributes={stringify(attributes)}>
+					is loading ...
+				</div>
+			)
+		},
+		migrate: (attributes: { blockName: string }) => {
+			const { blockName, ...rest } = attributes
+			return {
+				...rest,
+				metadata: {
+					name: blockName,
+				},
+			}
+		},
+		isEligible: (attributes: { blockName: string }) => typeof attributes.blockName === 'string',
+	}
+}
