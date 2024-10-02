04ff6a3275563ee245cff434f29c62d626bf119d
Arooran Thanabalasingam <13069024+bigaru@users.noreply.github.com>
2024-10-02 15:14:54 +0200

    feat: add import function

--- src/globalScript/lighthouse/index.tsx
+++ src/globalScript/lighthouse/index.tsx
@@ -4,6 +4,7 @@ import { usePublish, Actions } from './usePublish'
 import { useWatch } from './useWatch'
 import { exportAsJson } from './export'
 import type { Nucleus } from './types'
+import './import'
 
 export { InseriRoot, useDiscover, usePublish, useWatch, exportAsJson }
 export type { Actions, Nucleus, DiscoveredItem }
