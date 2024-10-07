2a6396b6a27e1cc25a841acc98bb84eb7bafc14d
Arooran Thanabalasingam <13069024+bigaru@users.noreply.github.com>
2024-10-07 10:14:41 +0200

    feat: add useRestorableState

--- src/globalScript/lighthouse/index.tsx
+++ src/globalScript/lighthouse/index.tsx
@@ -3,8 +3,9 @@ import { useDiscover, DiscoveredItem } from './useDiscover'
 import { usePublish, Actions } from './usePublish'
 import { useWatch } from './useWatch'
 import { exportAsJson } from './export'
+import { useRestorableState } from './useRestorableState'
 import type { Nucleus } from './types'
 import './import'
 
-export { InseriRoot, useDiscover, usePublish, useWatch, exportAsJson }
+export { InseriRoot, useDiscover, usePublish, useWatch, exportAsJson, useRestorableState }
 export type { Actions, Nucleus, DiscoveredItem }
