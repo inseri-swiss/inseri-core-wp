8668a16a49179ebe4afa3f9ef91737b2ca46b520
Arooran Thanabalasingam <13069024+bigaru@users.noreply.github.com>
2024-10-16 15:42:33 +0200

    feat: exclude Uint8Array from export

--- src/globalScript/lighthouse/export.tsx
+++ src/globalScript/lighthouse/export.tsx
@@ -4,7 +4,7 @@ export function exportAsJson(): string {
 	const blockEntries = Object.entries(restorableDataRoot)
 		.filter(([_, block]) => Object.values(block).length > 0)
 		.map(([blockId, block]) => {
-			const entries = Object.entries(block).filter(([_, slice]) => !(slice instanceof Blob))
+			const entries = Object.entries(block).filter(([_, slice]) => !(slice instanceof Blob) && !(slice instanceof Uint8Array))
 
 			return [blockId, Object.fromEntries(entries)]
 		})
