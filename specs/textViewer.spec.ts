5688609a3e4664c758c9da05915051f22032d7c1
Arooran Thanabalasingam <13069024+bigaru@users.noreply.github.com>
2024-11-14 16:19:28 +0100

    test: create fixture step-by-step instead of xml data

--- specs/textViewer.spec.ts
+++ specs/textViewer.spec.ts
@@ -1,33 +1,94 @@
 import { test, expect } from '@wordpress/e2e-test-utils-playwright'
-import { Post, getPostUrl } from './helper'
 
 const viewerSelector = '.wp-block-inseri-core-text-viewer'
 const editorSelector = '.wp-block-inseri-core-text-editor'
 
 test.describe('TextViewer', () => {
-	test('should have title and icon in editor ', async ({ admin, editor, page }) => {
+	test('should have title, icon, label, text in editor ', async ({ admin, editor, page }) => {
 		await admin.createNewPost()
 		await editor.insertBlock({ name: 'inseri-core/text-viewer' })
 
 		const block = page.locator(viewerSelector).first()
 		expect(block.getByText('Text Viewer')).toBeVisible()
 		expect(block.getByRole('img').first()).toHaveClass(/tabler-icon-file-typography/)
+
+		await block.click()
+		await page.getByLabel('Label').fill('MyViewer')
+
+		await page.getByPlaceholder('Search for blocks, content type,').click()
+		await page.getByRole('option', { name: 'core - blocks JSON inseri' }).click()
+
+		await expect(block.getByText('MyViewer')).toBeVisible()
+		await expect(block.getByRole('img').filter()).toHaveClass(/tabler-icon-eye/)
+		await expect(block.locator('textarea').first()).toHaveText(/"blockName"/)
 	})
 
-	test('should have label and icon when published', async ({ page }) => {
-		await page.goto(getPostUrl(Post.TextViewer_TextEditor))
-		const block = page.locator(viewerSelector).first()
-		expect(block.getByText('MyViewer')).toBeVisible()
-		expect(block.getByRole('img').filter()).toHaveClass(/tabler-icon-eye/)
-	})
+	test('should have label, icon, text when published', async ({ admin, editor, page }) => {
+		await admin.createNewPost()
 
-	test('should receive new inputs', async ({ page }) => {
-		await page.goto(getPostUrl(Post.TextViewer_TextEditor))
-		const viewerBlock = page.locator(viewerSelector).first()
+		await editor.insertBlock({ name: 'inseri-core/text-viewer' })
+		await editor.insertBlock({ name: 'inseri-core/text-editor' })
+
+		let viewerBlock = page.locator(viewerSelector).first()
 		const editorBlock = page.locator(editorSelector).first()
+
+		await viewerBlock.click()
+		await page.getByLabel('Label').fill('MyViewer')
+
+		await editorBlock.getByLabel('Choose a format').click()
+		await editorBlock.getByRole('option', { name: 'Text' }).click()
+
+		await viewerBlock.getByPlaceholder('Search for blocks, content type,').click()
+		await viewerBlock.getByText('Text Editor').click()
+
+		await editorBlock.locator('textarea').first().fill('hello world')
+
+		await editor.publishPost()
+		const newPage = await editor.openPreviewPage()
+
+		viewerBlock = newPage.locator(viewerSelector).first()
+
+		await expect(viewerBlock.getByText('MyViewer')).toBeVisible()
+		await expect(viewerBlock.getByRole('img').filter()).toHaveClass(/tabler-icon-eye/)
+		await expect(viewerBlock.locator('textarea').first()).toHaveText('hello world')
+
+		await newPage.close()
+	})
+
+	test('should receive new inputs', async ({ admin, editor, page }) => {
+		await admin.createNewPost()
+
+		await editor.insertBlock({ name: 'inseri-core/text-viewer' })
+		await editor.insertBlock({ name: 'inseri-core/text-editor' })
+
+		let viewerBlock = page.locator(viewerSelector).first()
+		let editorBlock = page.locator(editorSelector).first()
+
+		await viewerBlock.click()
+		await page.getByLabel('Label').fill('MyViewer')
+
+		await editorBlock.click()
+		await page.getByLabel('Label').fill('MyEditor')
+		await page.getByLabel('publicly editable').check()
+
+		await editorBlock.getByLabel('Choose a format').click()
+		await editorBlock.getByRole('option', { name: 'Text' }).click()
+
+		await viewerBlock.getByPlaceholder('Search for blocks, content type,').click()
+		await viewerBlock.getByText('Text Editor').click()
+
+		await editorBlock.locator('textarea').first().fill('hello world')
+
+		await editor.publishPost()
+		const newPage = await editor.openPreviewPage()
+
+		viewerBlock = newPage.locator(viewerSelector).first()
+		editorBlock = newPage.locator(editorSelector).first()
 
 		await editorBlock.locator('textarea').first().fill('Hola Mundo!')
 		await page.waitForTimeout(1000) // explicit wait needed
-		expect(viewerBlock.locator('textarea').first()).toHaveText('Hola Mundo!')
+		await expect(viewerBlock.locator('textarea').first()).toHaveText('Hola Mundo!')
+
+		await newPage.close()
 	})
 })
