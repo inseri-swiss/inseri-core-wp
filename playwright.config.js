8849718a72b29a31bc48faf421f3b6b1410cbf73
Andrei V. Plamada <andrei.plamada@uzh.ch>
2025-03-06 14:41:45 +0100

    ci: increase expect timeout to 10_000

--- playwright.config.js
+++ playwright.config.js
@@ -4,5 +4,9 @@ import { defineConfig } from '@playwright/test'
 const baseConfig = require('@wordpress/scripts/config/playwright.config.js')
 const config = defineConfig({
 	...baseConfig,
+	expect: {
+		...baseConfig.expect,
+		timeout: 10_000,
+	},
 })
 export default config
